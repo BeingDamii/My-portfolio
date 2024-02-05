692a928f6053077eea0cbc422dd86313f5fee0dd
Damii dennison <dwanedehydra@gmail.com>
2024-02-05 01:30:40 +0100

    fixed the types issue

--- app/Page-components/ProjectCard.tsx
+++ app/Page-components/ProjectCard.tsx
@@ -1,17 +1,19 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import FPimgOne from "../assets/FP-img-1.png"
 
-const ProjectCard = ({subHeading}) => {
+interface ProjectCardProps {
+  subHeading: string;
+  imageSrc: StaticImageData; // Assuming FPimgOne is a string representing the image URL
+}
+
+const ProjectCard: React.FC<ProjectCardProps> = ({ subHeading, imageSrc }) => {
   return (
     <div className="project-card">
-      <Image src={FPimgOne} alt="" />
+      <Image src={imageSrc} alt="" /> 
       <div className="project-heading-wrapper">
-        <h2 className="subheading">
-          {subHeading}
-        </h2>
+        <h2 className="subheading">{subHeading}</h2>
       </div>
     </div>
   );
 };
-
 export default ProjectCard;
--- app/Page-components/featuredProjects.tsx
+++ app/Page-components/featuredProjects.tsx
@@ -9,11 +9,23 @@ const FeaturedProjects = () => {
         <h1>Featured projects.</h1>
       </div>
       <div className="project-card-wrapper">
-        <ProjectCard subHeading="Building an online results verifier to combat election rigging in the
-          2023 Nigerian national elections." />
-        <ProjectCard subHeading="Redesigning VEFI's NFT marketplace to serve 8,000 community members." />
-        <ProjectCard subHeading="Web-app to showcase various places to visit around Africa." />
-        <ProjectCard subHeading="Curating my favorite Beephop Lo-Fi sounds into an online playlist" />
+        <ProjectCard
+          imageSrc={FPimgOne}
+          subHeading="Building an online results verifier to combat election rigging in the
+          2023 Nigerian national elections."
+        />
+        <ProjectCard
+          imageSrc={FPimgOne}
+          subHeading="Redesigning VEFI's NFT marketplace to serve 8,000 community members."
+        />
+        <ProjectCard
+          imageSrc={FPimgOne}
+          subHeading="Web-app to showcase various places to visit around Africa."
+        />
+        <ProjectCard
+          imageSrc={FPimgOne}
+          subHeading="Curating my favorite Beephop Lo-Fi sounds into an online playlist"
+        />
       </div>
     </div>
   );
