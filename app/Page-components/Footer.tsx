0f5164d0b467a6fb4d785c554934c29c6e73fceb
Damii dennison <dwanedehydra@gmail.com>
2024-02-07 01:11:24 +0100

    ****started mobile styling ****

--- app/Page-components/Footer.tsx
+++ app/Page-components/Footer.tsx
@@ -3,30 +3,30 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
+    <footer>
       <div className="footer-heading-container">
         <h3 className="footer-heading">Check out more of my stuff...</h3>
       </div>
       <div className="footer-links-wrapper">
         <div className="footer-link-group">
-          <Link href="/" className="footer-link">
+          <Link href="https://dribbble.com/damiiisgod" target="_blank" className="footer-link">
             Dribbble
           </Link>
-          <Link href="/" className="footer-link">
+          <Link href="https://github.com/BeingDamii" target="_blank" className="footer-link">
             Github
           </Link>
-          <Link href="/" className="footer-link">
+          <Link href="https://twitter.com/hydedenz" target="_blank" className="footer-link">
             Twitter
           </Link>
         </div>
         <div className="footer-link-group">
-          <Link href="/" className="footer-link">
+          <Link href="mailto:dwanedehydra@gmail.com" target="_blank" className="footer-link">
             E-Mail
           </Link>
-          <Link href="/" className="footer-link">
+          <Link href="https://www.linkedin.com/in/damii-dennison-aa7975255/" target="_blank" className="footer-link">
             Linkedin
           </Link>
-          <Link href="/" className="footer-link">
+          <Link href="https://medium.com/@hydedenz" target="_blank" className="footer-link">
             Medium
           </Link>
         </div>
@@ -36,7 +36,7 @@ const Footer = () => {
           This website was built in 2 sprints here is <Link href="/">how i did it.</Link>
         </p>
       </div>
-    </div>
+    </footer>
   );
 };
 
